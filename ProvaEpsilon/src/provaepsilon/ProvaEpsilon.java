1d175c45d1a04b72926a6c3d88f3c33d5f4c964d
lucad93 <>
2015-04-01 15:36:36 +0200

    prova2

--- ProvaEpsilon/src/provaepsilon/ProvaEpsilon.java
+++ ProvaEpsilon/src/provaepsilon/ProvaEpsilon.java
@@ -11,12 +11,5 @@ package provaepsilon;
  */
 public class ProvaEpsilon {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("");
-    }
     
 }
