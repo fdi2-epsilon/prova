8e7686988cdc65d9e854552fe25d7d68f350f502
lucad93@outlook.it <>
2015-04-12 17:40:21 +0200

    prova

---
+++ cri1/src/cri1/Cri1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cri1;
+
+/**
+ *
+ * @author Luca
+ */
+public class Cri1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Qui non sta più funzionando un emerito cazzo di niente!");
+    }
+    
+}
