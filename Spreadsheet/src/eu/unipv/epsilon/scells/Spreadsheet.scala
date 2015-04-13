4cbbd564a861bd0cf40e8de6b51039b0b033df77
Luca Zanussi <luca.z@outlook.com>
2015-04-13 20:51:11 +0200

    Table cells are now redrawn on update

--- Spreadsheet/src/eu/unipv/epsilon/scells/Spreadsheet.scala
+++ Spreadsheet/src/eu/unipv/epsilon/scells/Spreadsheet.scala
@@ -1,7 +1,6 @@
 package eu.unipv.epsilon.scells
 
-import swing._
-import event._
+import swing._, event._
 
 class Spreadsheet(val height: Int, val width: Int) extends ScrollPane {
 
@@ -9,18 +8,14 @@ class Spreadsheet(val height: Int, val width: Int) extends ScrollPane {
   import cellModel._
 
   val table = new Table(height, width) {
+
     rowHeight = 25
     autoResizeMode = Table.AutoResizeMode.Off
     showGrid = true
     gridColor = new java.awt.Color(150, 150, 150)
 
-    reactions += {
-      case TableUpdated(table, rows, column) =>
-        for (row <- rows) cells(row)(column).formula = FormulaParsers.parse(userData(row, column))
-    }
-
-    override def rendererComponent(isSelected: Boolean, focused: Boolean, row: Int, column: Int): Component =
-      if (focused) new TextField(userData(row, column))
+    override def rendererComponent(isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int): Component =
+      if (hasFocus) new TextField(userData(row, column))
       else new Label(cells(row)(column).toString) {
         xAlignment = Alignment.Right
       }
@@ -31,6 +26,17 @@ class Spreadsheet(val height: Int, val width: Int) extends ScrollPane {
       val v = this(row, column) // Not constructor invocation but apply()!
       if (v == null) "" else v.toString
     }
+
+    reactions += {
+      case TableUpdated(table, rows, column) =>
+        for (row <- rows) cells(row)(column).formula = FormulaParsers.parse(userData(row, column))
+      case ValueChanged(cell) =>
+        updateCell(cell.row, cell.column)
+    }
+
+    // Listen to all cells for value changes
+    for (row <- cells; cell <- row) listenTo(cell)
+
   }
 
   val rowHeader = new ListView((0 until height) map (_.toString)) {
