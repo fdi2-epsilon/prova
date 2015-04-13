a6266e57077cdf350a38f790e6cb226b2202641d
Luca Zanussi <luca.z@outlook.com>
2015-04-13 20:25:44 +0200

    Cells now listen for changed values and update accordingly

--- Spreadsheet/src/eu/unipv/epsilon/scells/Model.scala
+++ Spreadsheet/src/eu/unipv/epsilon/scells/Model.scala
@@ -1,15 +1,44 @@
 package eu.unipv.epsilon.scells
 
+import swing._
+
 class Model(val height: Int, val width: Int) extends Evaluator with Arithmetic {
 
-  case class Cell(row: Int, column: Int) {
-    var formula: Formula = Empty
-    def value = evaluate(formula)
+  case class Cell(row: Int, column: Int) extends Publisher {
+
     override def toString = formula match {
       case Textual(s) => s
       case _ => value.toString // Show evaluation result
     }
+
+    // The 'value' setter does the publishing...
+    private var v: Double = 0
+    def value: Double = v
+    def value_=(w: Double): Unit = {
+      if (!(v == w || v.isNaN && w.isNaN)) {
+        // NaN needs to be treated separately since NaN != NaN
+        v = w
+        publish(ValueChanged(this))
+      }
+    }
+
+    // ...and the 'formula' setter the subscribing.
+    private var f: Formula = Empty
+    def formula: Formula = f
+    def formula_=(f: Formula): Unit = {
+      for (c <- references(formula)) deafTo(c)    // Unsubscribe from previous formula refs
+      this.f = f            // Assign the new formula
+      for (c <- references(formula)) listenTo(c)  // Subscribe to the refs of the new formula
+      value = evaluate(f)   // Recompute value with the new formula
+    }
+
+    reactions += {
+      case ValueChanged(cell: Cell) => value = evaluate(formula)
+    }
+
   }
+
+  case class ValueChanged(cell: Cell) extends event.Event
 
   val cells = Array.ofDim[Cell](height, width)
 
